53709480fe14d5d59743da21981e6e065ee118e4
1computer1 <mineox100@gmail.com>
2019-03-12 12:31:45 -0400

    Move index.js

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-const CompilerClient = require('./src/struct/CompilerClient');
+const CompilerClient = require('./struct/CompilerClient');
 const config = require('./config.json');
 const client = new CompilerClient(config);
 client.once('ready', () => console.log('Comp_iler ready to go!'));
