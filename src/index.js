5483c509c0a78ede4df85a99bab1333735ad2ab2
1computer1 <mineox100@gmail.com>
2019-03-12 12:51:39 -0400

    Disable eslint for a line

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const CompilerClient = require('./struct/CompilerClient');
 const config = require('../config.json');
 const client = new CompilerClient(config);
-client.once('ready', () => console.log('Comp_iler ready to go!'));
+client.once('ready', () => console.log('Comp_iler ready to go!')); // eslint-disable-line no-console
 client.start();
