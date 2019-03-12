c82f3963011c1bed69f406eb1004bab412734704
1computer1 <mineox100@gmail.com>
2019-03-12 12:49:35 -0400

    Update src/index.js

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 const CompilerClient = require('./struct/CompilerClient');
-const config = require('./config.json');
+const config = require('../config.json');
 const client = new CompilerClient(config);
 client.once('ready', () => console.log('Comp_iler ready to go!'));
 client.start();
