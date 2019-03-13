f106a5e27352fcd985ba868aeef1caad8992954f
1Computer1 <mineox100@gmail.com>
2019-03-12 23:02:21 -0400

    Update fsharp.js

--- src/languages/fsharp.js
+++ src/languages/fsharp.js
@@ -2,7 +2,7 @@ const Language = require('../struct/Language');
 
 class FSharp extends Language {
     constructor() {
-        super('FSharp', {
+        super('fsharp', {
             highlight: 'fs',
             aliases: ['fsharp', 'f#', 'fs']
         });
