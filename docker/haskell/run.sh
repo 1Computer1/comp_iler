9a9c2b3d9500e94fa1eb82792433f6abf6be197e
1computer1 <mineox100@gmail.com>
2019-05-16 22:13:47 -0400

    Simplify and speed up Haskell module running

--- docker/haskell/run.sh
+++ docker/haskell/run.sh
@@ -4,10 +4,7 @@ else
     mkdir "$COUNT" && cd "$COUNT"
 
     echo "$1" > program.hs
-    ghc -O0 -j +RTS -A128m -n2m -RTS \
-        -no-keep-o-files -no-keep-hi-files \
-        program.hs >/dev/null \
-        && ./program
-    
+    ghc -e main program.hs
+
     cd .. && rm -rf "$COUNT"
 fi
