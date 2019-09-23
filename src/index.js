91f5e9c8cd45e4b511106d656e965338f3349ce5
1Computer1 <22125769+1Computer1@users.noreply.github.com>
2019-09-23 12:53:48 -0400

    Give bot a presence

--- src/index.js
+++ src/index.js
@@ -1,5 +1,14 @@
 const CompilerClient = require('./struct/CompilerClient');
 const config = require('../config.json');
 const client = new CompilerClient(config);
-client.once('ready', () => console.log('Comp_iler ready to go!')); // eslint-disable-line no-console
+client.once('ready', () => {
+    client.user.setPresence({
+        activity: { name: '@Comp_iler help' },
+        status: 'online'
+    });
+
+    // eslint-disable-next-line no-console
+    console.log('Comp_iler ready to go!');
+});
+
 client.start();
